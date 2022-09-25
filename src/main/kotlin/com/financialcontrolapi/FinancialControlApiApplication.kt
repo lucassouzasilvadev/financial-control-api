6a6694f8873d8d661d484b1a887917ff438d6a06
lucassouzasilvadev <lucasdrop169@gmail.com>
2022-09-25 20:25:09 -0300

    feat: subindo estrutura inicial do projeto

---
+++ src/main/kotlin/com/financialcontrolapi/FinancialControlApiApplication.kt
@@ -0,0 +1,11 @@
+package com.financialcontrolapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FinancialControlApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<FinancialControlApiApplication>(*args)
+}
