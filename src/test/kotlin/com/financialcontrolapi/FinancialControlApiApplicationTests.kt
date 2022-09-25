193508c244da50db2492af26257be09c591ecc51
lucassouzasilvadev <lucasdrop169@gmail.com>
2022-09-25 20:34:46 -0300

    adicionando um comentario

--- src/test/kotlin/com/financialcontrolapi/FinancialControlApiApplicationTests.kt
+++ src/test/kotlin/com/financialcontrolapi/FinancialControlApiApplicationTests.kt
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest
 @SpringBootTest
 class FinancialControlApiApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
+//	@Test
+//	fun contextLoads() {
+//	}
 
 }
