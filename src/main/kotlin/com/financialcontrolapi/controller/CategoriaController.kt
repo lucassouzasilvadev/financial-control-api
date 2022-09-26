dbdb436a83a349033dfb92ff1ba7e7c3f015eb87
lucassouzasilvadev <lucasdrop169@gmail.com>
2022-09-25 22:08:41 -0300

    feat: criando controller e endpoint para listar todas as categorias

--- src/main/kotlin/com/financialcontrolapi/controller/CategoriaController.kt
+++ src/main/kotlin/com/financialcontrolapi/controller/CategoriaController.kt
@@ -1,4 +1,18 @@
 package com.financialcontrolapi.controller
 
-class CategoriaController {
+import com.financialcontrolapi.model.Categoria
+import com.financialcontrolapi.repository.CategoriaRepository
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("v1/categorias")
+class CategoriaController (private val repository: CategoriaRepository){
+
+    @GetMapping
+    fun listar(): List<Categoria> {
+        return repository.findAll()
+    }
+
 }
